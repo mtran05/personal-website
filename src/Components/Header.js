85eeb928e16e548fb504ac27b64a98a353b4c7c9
Mason <tranquocthinh2912@gmail.com>
2024-01-21 13:09:49 -0500

    testing

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import pfp from '../Images/pfp.jpg';
+
+function Header() {
+    return (
+        <header>
+            <img src={pfp}></img>
+        </header>
+    );
+}
+
+export default Header;
